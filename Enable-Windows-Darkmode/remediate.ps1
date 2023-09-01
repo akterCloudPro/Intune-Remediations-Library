f16d8b7a76d28c5b43c4d178ecb2f94a02a7b539
Niklas Rast <47035125+niklasrast@users.noreply.github.com>
2023-09-01 21:22:04 +0200

    Update remediate.ps1

--- Enable-Windows-Darkmode/remediate.ps1
+++ Enable-Windows-Darkmode/remediate.ps1
@@ -1,7 +1,9 @@
 $regpath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize"
 $regname = "AppsUseLightTheme"
+$regname1 = "SystemUsesLightTheme"
 $regvalue = "0"
 $regtype = "DWORD"
 
 
-New-ItemProperty -LiteralPath $regpath -Name $regname -Value $regvalue -PropertyType $regtype -Force -ea SilentlyContinue;
+New-ItemProperty -LiteralPath $regpath -Name $regname -Value $regvalue -PropertyType $regtype -Force -ea SilentlyContinue
+New-ItemProperty -LiteralPath $regpath -Name $regname1 -Value $regvalue -PropertyType $regtype -Force -ea SilentlyContinue
