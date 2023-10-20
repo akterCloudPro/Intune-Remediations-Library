859e8b4ce27083945f46302cf7d9e0e58593b141
Niklas Rast <47035125+niklasrast@users.noreply.github.com>
2023-10-20 15:01:16 +0200

    Update detect-cmtrace.ps1

--- Install-CMTrace/detect-cmtrace.ps1
+++ Install-CMTrace/detect-cmtrace.ps1
@@ -1,17 +1,3 @@
-<#
-Version: 1.0
-Author: 
-- Joey Verlinden (joeyverlinden.com)
-- Andrew Taylor (andrewstaylor.com)
-- Florian Slazmann (scloud.work)
-- Jannik Reinhard (jannikreinhard.com)
-Script: detect-cmtrace.ps1
-Description: Detects if CMTrace is installed
-Hint: This is a community script. There is no guarantee for this. Please check thoroughly before running.
-Version 1.0: Init
-Run as: System
-Context: 64 Bit
-#> 
 $Path = "c:\windows\system32\cmtrace.exe"
 
 Try {
