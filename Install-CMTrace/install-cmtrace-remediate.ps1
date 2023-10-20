499933409c9b8777d5bc41b6876c6f8d9c910403
Niklas Rast <47035125+niklasrast@users.noreply.github.com>
2023-10-20 12:23:11 +0200

    Update install-cmtrace-remediate.ps1

--- Install-CMTrace/install-cmtrace-remediate.ps1
+++ Install-CMTrace/install-cmtrace-remediate.ps1
@@ -16,3 +16,10 @@ Context: 64 Bit
 $ownRepoUri = "https://github.com/niklasrast/niklasrast/raw/main/CMTrace.exe"
 
 invoke-webrequest -uri $ownRepoUri -outfile "C:\Windows\System32\cmtrace.exe"
+
+New-Item -Path HKLM:\SOFTWARE\Classes\.lo_ -Value "Log.File" -Force -ErrorAction SilentlyContinue
+New-Item -Path HKLM:\SOFTWARE\Classes\.log -Value "Log.File" -Force -ErrorAction SilentlyContinue
+New-Item -Path HKLM:\SOFTWARE\Classes\Log.File\shell\open\command -Value "`"C:\Program Files (x86)\IntuneSystemTrayV2\CMTrace.exe`" `"%1`"" -Force -ErrorAction SilentlyContinue
+New-Item -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\CMtrace" -Force -ErrorAction SilentlyContinue
+New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\CMtrace" -Name "Version" -Value 1 -PropertyType String -Force -ErrorAction SilentlyContinue
+New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\CMtrace" -Name "StubPath" -Value "reg.exe ADD HKCU\Software\Microsoft\Trace32 /v ""Register File Types"" /d 0 /f" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue
